575a31c4ca572633739880993c4b36c86d50968b
Ivan Salov <ivan.v.salov@gmail.com>
2022-08-22 19:11:44 +0300

    init: start task codejam

---
+++ script.js
@@ -0,0 +1,3 @@
+import {difficulties} from './data/difficulties.js';
+import {ancientsData} from './data/ancients.js';
+import {brownCards, blueCards, greenCards} from './data/mythicCards/index.js';
