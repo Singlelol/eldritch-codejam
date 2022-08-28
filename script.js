8e42899bb3a5d9be2ec8a1bc0d01d40e5415504b
singlelol <singlelol21@gmail.com>
2022-08-28 12:50:04 +0300

    fix: edit index.js

--- script.js
+++ script.js
@@ -49,3 +49,40 @@ drawCard(name[key]);
 let firstStage = [ancientsData[0].firstStage.greenCards, ancientsData[0].firstStage.blueCards, ancientsData[0].firstStage.brownCards]
 let secondStage = [ancientsData[0].secondStage.greenCards, ancientsData[0].secondStage.blueCards, ancientsData[0].secondStage.brownCards]
 let thirdStage = [ancientsData[0].thirdStage.greenCards, ancientsData[0].thirdStage.blueCards, ancientsData[0].thirdStage.brownCards]
+
+let firstStageCard = [];
+let secondStageCard = [];
+let thirdStageCard = [];
+
+function sortCard(arr, result) {
+  for (let i = 0; i < arr.length; i++) {
+    result.push(deck[i].slice(0, arr[i]));
+    deck[i].splice(0, arr[i])
+  }
+return result;
+}
+
+let rollCard = [firstStageCard, secondStageCard, thirdStageCard]
+
+sortCard(firstStage, firstStageCard);
+sortCard(secondStage, secondStageCard);
+sortCard(thirdStage, thirdStageCard);
+
+function pullCard() {
+  function random(min, max) {
+return Math.floor(Math.random() * (max - min + 1) + min);
+}
+rollCard = [firstStageCard, []];
+let index = random(0, rollCard[0].length - 1);
+if (rollCard[0].length === 0) {
+  rollCard.shift()
+}
+if (rollCard[0][index].length === 0) {
+  rollCard[0].splice([index], 1)
+}
+if (rollCard.length === 0) {
+  return;
+}
+console.log(rollCard[0][index].shift())
+}
+pullCard()
