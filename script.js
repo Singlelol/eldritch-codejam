7ed36564b79b04dd87fb00b9ddf5f8c68ff112c7
Ivan Salov <ivan.v.salov@gmail.com>
2022-08-28 23:32:45 +0300

    fix: edit script.js

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ const godCard = document.querySelectorAll('.gameland__immortal-item');
 const startButton = document.querySelector('.gameland__level-start');
 const backCard = document.querySelector('.gameland__playCards-deck');
 const frontCard = document.querySelector('.gameland__playCards-card');
+const mode = document.querySelector('.gameland__level-items');
+const modeChildren = mode.querySelectorAll('.gameland__level-item');
 
 let name = {
     greenCards: 0,
     blueCards: 0,
     brownCards:0,
     };
+
+let ancientsIndex = 0;
   
   function search(arrs, elem) {
     const colorElem = elem;
@@ -23,20 +27,22 @@ let name = {
   return name[colorElem]
   }
   
-  function countCard() {
+  function countCard(num) {
     name = {
       greenCards: 0,
       blueCards: 0,
       brownCards:0,
       };
     for (let prop in name) {
-        search(ancientsData[0], prop);
+        search(ancientsData[num], prop);
       };
+  return ancientsIndex = num;
   }
 
   godCard.forEach((el, index) => {
     el.addEventListener('click', ()=> {
-      countCard();
+      console.log(index);
+      countCard(index);
       for (const [key, value] of Object.entries(name)) {
         console.log(`${key}: ${value}`);
       }
@@ -45,10 +51,34 @@ let name = {
 
 let deck;
 
-function startGame() {
+function startGame(num) {
   deck = [];
-  for (let key in name) { 
+  console.log(num)
+  for (let key in name) {
     let cardsAmount = [...greenCards, ...blueCards, ...brownCards].filter(element => element.color === key.slice(0, -5));
+    console.log(cardsAmount)
+    if (num === 1) {
+      cardsAmount = cardsAmount.filter(element => element.difficulty !== 'hard')
+      console.log(cardsAmount)
+    } else if (num === 3) {
+      cardsAmount = cardsAmount.filter(element => element.difficulty !== 'easy')
+      console.log(cardsAmount)
+    } else if (num === 0) {
+      let result = cardsAmount.filter(element => element.difficulty === 'easy')
+      if(result.length < name.key) {
+          let coun = cardsAmount.filter(element => element.difficulty === 'normal')
+          result.concat(coun.slice(0, name.key - result.length))
+        }
+        cardsAmount = result;
+      } else if (num === 4) {
+        let result = cardsAmount.filter(element => element.difficulty === 'hard')
+        if(result.length < name.key) {
+            let coun = cardsAmount.filter(element => element.difficulty === 'normal')
+            result.concat(coun.slice(0, name.key - result.length))
+          }
+          cardsAmount = result;
+        }
+    console.log(cardsAmount)
     function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
@@ -66,28 +96,40 @@ function startGame() {
   return deck;
 }
 
+
 startButton.addEventListener('click', () => {
-  startGame();
-  sortCard(firstStage, firstStageCard);
-  sortCard(secondStage, secondStageCard);
-  sortCard(thirdStage, thirdStageCard);
-  rollCard = [firstStageCard, secondStageCard, thirdStageCard];
-  console.log(firstStage)
-  console.log(secondStage)
-  console.log(thirdStage)
-  console.log(firstStageCard)
-  console.log(secondStageCard)
-  console.log(thirdStageCard)
-  console.log(deck)
+startButton.classList.add('none');
+mode.classList.remove('none');
+modeChildren.forEach((el, index) => {
+  el.textContent = difficulties[index].name;
+}
+)
 })
 
-let firstStage = [ancientsData[0].firstStage.greenCards, ancientsData[0].firstStage.blueCards, ancientsData[0].firstStage.brownCards]
-let secondStage = [ancientsData[0].secondStage.greenCards, ancientsData[0].secondStage.blueCards, ancientsData[0].secondStage.brownCards]
-let thirdStage = [ancientsData[0].thirdStage.greenCards, ancientsData[0].thirdStage.blueCards, ancientsData[0].thirdStage.brownCards]
-
-let firstStageCard = [];
-let secondStageCard = [];
-let thirdStageCard = [];
+modeChildren.forEach((el, index) => {
+  el.addEventListener('click', () => {
+    let firstStage = [ancientsData[ancientsIndex].firstStage.greenCards, ancientsData[ancientsIndex].firstStage.blueCards, ancientsData[ancientsIndex].firstStage.brownCards]
+    let secondStage = [ancientsData[ancientsIndex].secondStage.greenCards, ancientsData[ancientsIndex].secondStage.blueCards, ancientsData[ancientsIndex].secondStage.brownCards]
+    let thirdStage = [ancientsData[ancientsIndex].thirdStage.greenCards, ancientsData[ancientsIndex].thirdStage.blueCards, ancientsData[ancientsIndex].thirdStage.brownCards]
+    
+    let firstStageCard = [];
+    let secondStageCard = [];
+    let thirdStageCard = [];
+      startGame(index);
+      sortCard(firstStage, firstStageCard);
+      sortCard(secondStage, secondStageCard);
+      sortCard(thirdStage, thirdStageCard);
+      rollCard = [firstStageCard, secondStageCard, thirdStageCard];
+      console.log(firstStage)
+      console.log(secondStage)
+      console.log(thirdStage)
+      console.log(firstStageCard)
+      console.log(secondStageCard)
+      console.log(thirdStageCard)
+      console.log(deck)
+    })
+}
+)
 let rollCard;
 
 function sortCard(arr, result) {
@@ -102,23 +144,26 @@ return result;
 }
 
 function pullCard() {
-  if (rollCard.length === 0 || !rollCard.length) {
+  console.log(rollCard.length)
+  if (!rollCard.length || rollCard.length === 0) {
     return;
   }
   function random(min, max) {
 return Math.floor(Math.random() * (max - min + 1) + min);
 }
 let index = random(0, rollCard[0].length - 1);
-if (rollCard[0][index].length) {
+console.log(rollCard[0][index].length)
+  console.log(rollCard[0].length)
+if (rollCard[0][index].length > 0) {
   let result = rollCard[0][index].shift()
-  if (rollCard[0][index].length === 0 || !rollCard[0][index].length) {
+  if (!rollCard[0][index].length || rollCard[0][index].length === 0) {
     rollCard[0].splice([index], 1)
+  }
+  if (!rollCard[0].length || rollCard[0].length === 0) {
+    rollCard.shift()
   }
   console.log(result)
   console.log(result[0].cardFace)
-  if (rollCard[0].length === 0 || !rollCard[0].length) {
-    rollCard.shift()
-  }
   return result[0].cardFace;
 } else {
   rollCard[0].splice([index], 1)
